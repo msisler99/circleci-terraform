8da6d21c906f6d9e2f7e557c3c8347408e942fa8
msisler99 <32968954+msisler99@users.noreply.github.com>
2018-10-17 15:37:07 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region     = "us-west-2"
 }
  
- resource "aws_instance" "example" {
+ resource "aws_instance" "Terraform" {
    ami            = "ami-0d1000aff9a9bad89"
    instance_type  = "t2.micro" 
    tags { 
-     Name = "example" 
+     Name = "Terraform" 
    } 
   } 
