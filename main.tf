bb2c27611d1f3c8a44340a082badcefea2884750
msisler99 <32968954+msisler99@users.noreply.github.com>
2018-10-17 15:26:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region     = "us-west-2"
 }
  
- resource "aws_vpc" "minimal" { 
-   cidr_block = "10.0.0.0/26" 
+ resource "aws_instance" "example" {
+   ami            = "ami-0d1000aff9a9bad89"
+   instance_type  = "t2.micro" 
    tags { 
-     Name = "minimal" 
+     Name = "example" 
    } 
   } 
