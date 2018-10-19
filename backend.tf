35c7bebf982397781d9da3f489c3e3aa011e5ecf
msisler99 <32968954+msisler99@users.noreply.github.com>
2018-10-19 10:32:28 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "${var.S3_bucket_name}"
     dynamodb_table = "${var.dynamodb_table_name}"
     key    = "${var.remote_state_file}"
-    region = "${var.aws_region]"
+    region = "${var.aws_region}"
   }
 }
