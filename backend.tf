2dbc7ebd08dc1babcf36e47dfeaf193db6187c42
Aaron Burt <aaron.burt@civicactions.com>
2018-11-21 10:21:52 -0800

    Can't do interpolation in backend config, supply via CLI instead

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "aburt-cammis-test"
     dynamodb_table = "aburt-cammis-test"
-    key            = "${var.state_path}/terraform.tfstate"
+#   key            = "${var.state_path}/terraform.tfstate"  # Supplied to "terraform init" command
     region         = "us-west-2"
   }
 }
