72fd54b5fbb071fd75b61a0602bb6995aebbc1c0
Aaron Burt <aaron.burt@civicactions.com>
2018-11-20 17:45:20 -0800

    fix broken formatting

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region     = "${var.aws_region}"
+  region = "${var.aws_region}"
 }
- 
- resource "aws_instance" "Terraform" {
-   ami            = "${var.AMI_Id}"
-   instance_type  = "${var.EC2_instance_type}" 
-   tags { 
-     Name = "Terraform" 
-   } 
-  } 
+
+resource "aws_instance" "Terraform" {
+  ami           = "${var.AMI_Id}"
+  instance_type = "${var.EC2_instance_type}"
+
+  tags {
+    Name = "Terraform"
+  }
+}
