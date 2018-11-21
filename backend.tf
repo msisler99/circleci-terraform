31f9a058ef78da0f962035753bd7d90363c14ca2
Aaron Burt <aaron.burt@civicactions.com>
2018-11-21 10:13:19 -0800

    Set state path in a TF var, pass via env var

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "aburt-cammis-test"
     dynamodb_table = "aburt-cammis-test"
-    key            = "GENERIC/terraform.tfstate"
+    key            = "${var.state_path}/terraform.tfstate"
     region         = "us-west-2"
   }
 }
